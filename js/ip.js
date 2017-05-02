db2b967aea6e0c217e5675d6dc0b781c928a6af3
Carles Serna <carles.serna@gmail.com>
2017-05-02 14:18:30 +0200

    ip angular

--- js/ip.js
+++ js/ip.js
@@ -1,4 +1,13 @@
-$.getJSON("http://freegeoip.net/json/", function (data) {
-    var country = data.country_name;
-    var ip = data.ip;
-});
+// $.getJSON("http://freegeoip.net/json/", function (data) {
+//     var country = data.country_name;
+//     var ip = data.ip;
+// });
+
+function GetCountry($scope, $http) {
+    $http.get('http://ipinfo.io/json').
+    success(function(data) {
+        $scope.location = data;
+        $scope.country = 'fr';
+        if (data.country != 'FR') $scope.country = "en";
+    });
+}
