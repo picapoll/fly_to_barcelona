007d04e2894ce503e42ef17357c2b775065ee94e
Carles Serna <carles.serna@gmail.com>
2017-05-03 09:55:36 +0200

    apikey fixed

--- js/service.js
+++ js/service.js
@@ -1,8 +1,8 @@
 angular.module('mainApp')
     .factory('skyscannerService', function($http) {
         function getCityOrigin(username) {
-        	// var apiKey = "sk858097278294566056945278099666"
-            var urlcountry =  'http://partners.api.skyscanner.net/apiservices/geo/v1.0?apikey=sk858097278294566056945278099666'
+            var apiKey = "sk858097278294566056945278099666"
+            var urlcountry =  'http://partners.api.skyscanner.net/apiservices/geo/v1.0?apikey=' + apiKey
             return $http.get(urlcountry)
 
         }
